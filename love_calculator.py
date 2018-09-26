51d6394d04fed27f4d0c317e251b090941426696
Deser12 <40196958+Deser12@users.noreply.github.com>
2018-09-26 12:33:31 +0200

    Create love_calculator.py

---
+++ love_calculator.py
@@ -0,0 +1,27 @@
+import random
+
+own_name = input("What's your name?")
+lover_name = input("What's your lovers name?")
+
+name_strip = own_name.strip()
+lover_strip = lover_name.strip() 
+
+name_lower = name_strip.lower()
+lover_lower = lover_strip.lower()
+
+name = len(name_lower)
+lover = len(lover_lower)
+
+random_low_number = random.randint(1, 33)
+random_mid_number = random.randint(34, 66) 
+random_high_number = random.randint(67, 100) 
+
+if name > lover:
+	print(str(random_low_number) +"%")
+elif name == lover: 
+	print(str(random_mid_number) +"%")
+else:
+	print(str(random_high_number) +"%")
+
+
+
